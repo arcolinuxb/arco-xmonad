8dd2d929519a37e57b970eb2de7de43f78e1196b
Erik Dubois <erik.dubois@gmail.com>
2020-12-06 08:29:13 +0100

    version number change

--- installation-scripts/40-build-the-iso-local-again.sh
+++ installation-scripts/40-build-the-iso-local-again.sh
@@ -29,7 +29,7 @@ echo
 	desktop="xmonad"
 	lightdmDesktop="xmonad"
 
-	arcolinuxVersion='v20.11.9'
+	arcolinuxVersion='v21.01.2'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
